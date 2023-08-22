a8b17cbb22ea016e520a4915618653b8b64d9d81
unknown <ttx8000@gmail.com>
2023-08-21 22:27:12 -0300

    first commit

---
+++ primeira_api/index.js
@@ -0,0 +1,25 @@
+const express = require("express") // Importa o módulo 'express'
+
+const app = express(); // Cria uma instância do aplicativo Express
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json())
+
+/*
+"app.get("/", (req, res) => { ... });: Aqui, você define uma rota GET para o caminho raiz ("/"). 
+Quando um cliente faz uma requisição GET para esse caminho, a função callback é executada. 
+Essa função recebe dois parâmetros: req (representando a requisição) e res (representando a resposta). 
+No exemplo, estamos enviando uma resposta JSON com uma mensagem "Primeira rota criada".
+*/
+app.get("/", (req, res) => {
+    res.json({
+        message: "Primeira rota criada"
+    })
+})
+
+app.listen(3000) //Isso faz com que o aplicativo Express escute na porta 3000
---
+++ status_na_resposta/index.js
@@ -0,0 +1,41 @@
+const express = require("express"); // Importa o módulo 'express'
+
+const app = express(); // Cria uma instância do aplicativo Express
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+//método post
+app.post("/createproduct", (req, res) => {
+  const name = req.body.name;
+  const price = req.body.price;
+
+  if(!name) {
+    res.status(422).json({message: "O campo nome é obrigatório!"})
+    return
+  }
+
+  console.log(name);
+  console.log(price);
+
+  res.status(201).json({message: `O produto ${name} foi criado com sucesso!`})
+});
+
+/*
+"app.get("/", (req, res) => { ... });: Aqui, você define uma rota GET para o caminho raiz ("/"). 
+Quando um cliente faz uma requisição GET para esse caminho, a função callback é executada. 
+Essa função recebe dois parâmetros: req (representando a requisição) e res (representando a resposta). 
+No exemplo, estamos enviando uma resposta JSON com uma mensagem "Primeira rota criada".
+*/
+app.get("/", (req, res) => {
+  res.json({
+    message: "Primeira rota criada",
+  });
+});
+
+app.listen(3000); //Isso faz com que o aplicativo Express escute na porta 3000
